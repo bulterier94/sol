8739b188725e980f1c6d96747a15a8a149959797
bulterier94 <cantsifst@gmail.com>
2026-03-03 23:08:37 +0300

    j

--- 8.sol
+++ 8.sol
@@ -2,17 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract Task_08 {
-   address public owner;
+    // Владелец контракта — адрес, который развернул контракт
+    address public owner;
 
-   constructor() {
-       owner = msg.sender;
-   }
+    constructor() {
+        owner = msg.sender;
+    }
 
-   function getBytes(uint256 _value) public pure returns (bytes memory) {
-       return abi.encodePacked(_value);
-   }
-
-   function getBytesFromString(string memory _str) public pure returns (bytes memory) {
-       return bytes(_str);
-   }
+    // Функция возвращает длину динамического массива байтов
+    function getDynamicByteArrayLength(bytes calldata data) public pure returns (uint) {
+        return data.length;
+    }
+    
+    // Функция возвращает байт по указанному индексу из динамического массива байтов
+    function getDynamicByteArrayElement(bytes calldata data, uint index) public pure returns (byte) {
+        // Проверка выхода за пределы массива
+        require(index < data.length, "Index out of bounds");
+        
+        return data[index];
+    }
 }
