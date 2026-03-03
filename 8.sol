5f1944fe7e9dc88c327f8cdb994f1c452d93e414
Максим Бухарин <113234278+bulterier94@users.noreply.github.com>
2026-03-03 23:12:17 +0300

    Update 8.sol

--- 8.sol
+++ 8.sol
@@ -1,24 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Task_08 {
-    // Владелец контракта — адрес, который развернул контракт
+contract Task_07 {
+
     address public owner;
 
     constructor() {
         owner = msg.sender;
     }
 
-    // Функция возвращает длину динамического массива байтов
-    function getDynamicByteArrayLength(bytes calldata data) public pure returns (uint) {
-        return data.length;
+    function getBytes(uint256 _value) public pure returns (bytes memory) {
+        return abi.encodePacked(_value);
     }
-    
-    // Функция возвращает байт по указанному индексу из динамического массива байтов
-    function getDynamicByteArrayElement(bytes calldata data, uint index) public pure returns (byte) {
-        // Проверка выхода за пределы массива
-        require(index < data.length, "Index out of bounds");
-        
-        return data[index];
+
+    // Функция возвращает длину динамического массива байтов
+    function getDynamicByteArrayLength(bytes memory _data) public pure returns (uint256) {
+        return _data.length;
+    }
+
+    function getDynamicByteArrayElement(bytes memory _data, uint256 _index) 
+        public 
+        pure 
+        returns (bytes1) 
+    {
+        require(_index < _data.length, "Index out of bounds");
+        return _data[_index];
     }
 }
